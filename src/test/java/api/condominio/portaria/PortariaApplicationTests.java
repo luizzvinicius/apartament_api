3ed111d9b424936b8b8ad83499f2fa8bc60d01f4
Luiz Vinicius <viniciusadr244@gmail.com>
2024-06-20 19:12:06 -0300

    first commit

---
+++ src/test/java/api/condominio/portaria/PortariaApplicationTests.java
@@ -0,0 +1,13 @@
+package api.condominio.portaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
