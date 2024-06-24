3ba1bfdd2d9768053f943d41f588d1918e0279de
Luiz Vinicius <viniciusadr244@gmail.com>
2024-06-24 09:14:22 -0300

    fix: name error

--- src/main/java/api/condominio/portaria/repository/ResidentRepository.java
+++ src/main/java/api/condominio/portaria/repository/ResidentRepository.java
@@ -1,10 +1,11 @@
 package api.condominio.portaria.repository;
 
+import api.condominio.portaria.models.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface Resident extends JpaRepository<Resident, UUID> {
+public interface ResidentRepository extends JpaRepository<Resident, UUID> {
 }
