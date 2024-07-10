9e808924f1da5fb3c18ad83d939dada6f5f85a6f
Luiz Vinicius <viniciusadr244@gmail.com>
2024-07-10 11:18:56 -0300

    feat: swagger docs

---
+++ src/main/java/api/condominio/portaria/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package api.condominio.portaria.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Apartament API specification"
+        )
+)
+public class OpenApiConfig {
+}
